de49bfb154b127be3f70d5323c4ba073b1823325
foresightyj <imyuanjian@gmail.com>
2019-07-30 18:30:47 +0800

    initial commit. added test.vue

---
+++ src/components/test.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>HI</span>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Prop } from "vue/types/options";
+export default Vue.extend({
+  props: {
+    TestId: {
+      require: true,
+      type: Number as Prop<number>
+    }
+  },
+  data() {
+    return {
+      showDialog: false
+    };
+  },
+  methods: {
+    handleSelectionChange(): void {
+      console.log(this.showDialog);
+    }
+  }
+});
+</script>
